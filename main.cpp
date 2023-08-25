f3a2644c1358cdfc9c93d5506dd35ac1be78e967
Matthew Fritsch <matthew@peanutrobotics.com>
2023-08-24 23:44:20 -0700

    initial commit created test.tsl started main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+struct option {
+    std::string optionName;
+    bool enabled;
+};
+
+int main(int argc, char** argv) {
+
+    
+
+    return 0;
+}
+
+std::vector<option> parseArgs(int argc, char** argv) {
+    std::vector<option> options;
+
+}
